4a8f1af6e5afa48b2289991025f33722f2b8a903
rnjailamba <rnjailamba12@gmail.com>
2017-08-16 18:40:43 +0530

    callback added

--- client.js
+++ client.js
@@ -1,25 +1,39 @@
-module.exports = {
-    escape: function(html) {
-        return String(html)
-            .replace(/&/g, '&amp;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
-    },
+var escape = function(html) {
+    return String(html)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
 
-    /**
-     * Unescape special characters in the given string of html.
-     *
-     * @param  {String} html
-     * @return {String}
-     */
-    unescape: function(html) {
-        return String(html)
-            .replace(/&amp;/g, '&')
-            .replace(/&quot;/g, '"')
-            .replace(/&#39;/g, ''')
-            .replace(/&lt;/g, '<')
-            .replace(/&gt;/g, '>');
-    }
+var unescape = function(html) {
+    return String(html)
+        .replace(/&amp;/g, '&')
+        .replace(/&quot;/g, '"')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>');
+}
+
+var callbackFn = function(callback) {
+    callback = callback || function () {}
+
+    return new Promise(function (resolve, reject) {
+        var data = {};
+        var err = false;
+        if (err) {
+            // reject as promise
+            reject(err)
+            // return callback using "error-first-pattern"
+            return callback(err)
+        }
+        resolve(data)
+        return callback(null, data)
+    })
+}
+
+module.exports = {
+    escape: escape,
+    unescape: unescape,
+    callbackFn: callbackFn
 };
